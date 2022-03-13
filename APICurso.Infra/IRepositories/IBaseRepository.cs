d230d3c6925367b471a1a3e7e608c1c4d8e5307f
rafael chaves <rafaelaugustochaves@hotmail.com>
2022-03-13 14:24:05 -0300

    melhoria em alguns imports em varias classes

---
+++ APICurso.Infra/IRepositories/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace APICurso.Infra.IRepositories
+{
+    interface IBaseRepository<T> where T : class
+    {
+        void Save(T obj);
+        void Save(List<T> obj);
+        T FindById(int id);
+        T Find(Expression<Func<T, bool>> predicate);
+        ICollection<T> Get();
+        ICollection<T> Get(Expression<Func<T, bool>> predicate);
+        bool Delete(T obj);
+        bool Delete(int id);
+        void Dispose();
+    }
+}
