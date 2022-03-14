667b0b0c6b27c50075f68851be7e5edcc0c78661
rafael chaves <rafaelaugustochaves@hotmail.com>
2022-03-13 21:42:28 -0300

    controller log implementada, falta realizar testes

---
+++ APICurso/Controllers/LogController.cs
@@ -0,0 +1,33 @@
+﻿using APICurso.Application.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICurso.Controllers
+{
+    public class LogController : Controller
+    {
+        private readonly ILogService logService;
+        public LogController(ILogService _logService)
+        {
+            logService = _logService;
+        }
+
+        [HttpGet]
+        [Route("ListarHistorico")]
+        public IActionResult ListarHistorico(int codigo, string nomeEntidade)
+        {
+            try
+            {
+                var log = logService.Get(r => r.IdEntitade == codigo && r.NomeEntidade == nomeEntidade).ToList().OrderBy(x => x.Id);
+                return Ok(log);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
